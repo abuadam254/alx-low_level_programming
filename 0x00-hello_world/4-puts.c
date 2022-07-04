757d9d11eca2d40cf504eb00d4de8aecc22f7d33
abuadam254 <ibrahim.mwabz@gmail.com>
2022-07-04 12:31:38 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Using puts() function
+ * End of comment
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
+
